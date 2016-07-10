c6a853614fe53623ea2b9f1ca433ed713e46b8f7
Ivan Reković <irekovic@gmail.com>
2016-07-11 01:42:09 +0200

    Adding router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import {Component} from '@angular/core'
-import {HeroesComponent} from './heroes.component'
+import {ROUTER_DIRECTIVES} from '@angular/router'
 import {HeroService} from './hero.service'
 
 @Component({
 	selector: 'my-app',
-	directives: [HeroesComponent],
+	directives: [ROUTER_DIRECTIVES],
 	providers: [HeroService],
 	template:`
 	<h1>{{title}}</h1>
-	<my-heroes></my-heroes>
+	<a [routerLink]="['/heroes']">Heroes</a>
+	<router-outlet></router-outlet>
 	`
 })
 export class AppComponent {
