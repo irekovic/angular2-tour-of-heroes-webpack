133f3a829a79a9b7615be48baa42e3402afe0f35
Ivan Reković <irekovic@gmail.com>
2016-07-11 01:22:41 +0200

    implementation that simulates slow fetch

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,18 @@ import {HEROES} from './mock-heroes'
 @Injectable()
 export class HeroService {
 	getHeroes() {
+		return this.getHeroesFast()
+	}
+
+	private getHeroesFast() {
 		return Promise.resolve(HEROES)
+	}
+
+	private getHeroesSlow() {
+		return new Promise<Hero[]>(resolve => 
+			setTimeout(() => 
+				resolve(HEROES), 2000
+			)
+		)
 	}
 }
