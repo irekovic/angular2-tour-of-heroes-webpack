da1f80aafd2af20d8195e87079d7a9aee165e177
Ivan Reković <irekovic@gmail.com>
2016-07-11 11:05:55 +0200

    setting up http heroservice with in-memory-data.service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,11 +1,19 @@
 import {Injectable} from '@angular/core'
-import {HEROES} from './mock-heroes'
+import {Http, Headers} from '@angular/http'
+import 'rxjs/add/operator/toPromise'
 import {Hero} from './hero'
 
 @Injectable()
 export class HeroService {
-	getHeroes() {
-		return this.getHeroesFast()
+	private heroesUrl = 'app/heroes'
+
+	constructor(private http: Http) {}
+
+	getHeroes(): Promise<Hero[]> {
+		return this.http.get(this.heroesUrl)
+			.toPromise()
+			.then(response => response.json().data)
+			.catch(this.handleError)
 	}
 
 	getHero(id: number) {
@@ -13,15 +21,54 @@ export class HeroService {
 			heroes.find(hero => hero.id === id)
 		)
 	}
-	private getHeroesFast() {
-		return Promise.resolve(HEROES)
+
+	save(hero: Hero): Promise<Hero> {
+		if (hero.id) return this.put(hero)
+		return this.post(hero)
 	}
 
-	private getHeroesSlow() {
-		return new Promise<Hero[]>(resolve => 
-			setTimeout(() => 
-				resolve(HEROES), 2000
-			)
-		)
+	delete(hero: Hero) {
+		let headers = new Headers()
+		headers.append('Content-Type', 'application/json')
+
+		let url = `${this.heroesUrl}/${hero.id}`
+
+		return this.http
+			.delete(url, headers)
+			.toPromise()
+			.catch(this.handleError)
+	}
+
+	private post(hero: Hero): Promise<Hero> {
+		let headers = new Headers({
+			'Content-Type':'application/json'
+		})
+
+		return this.http
+			.post(
+				this.heroesUrl, 
+				JSON.stringify(hero), 
+				{headers: headers}
+			).toPromise()
+			.then(res => res.json().data)
+			.catch(this.handleError)
+	}
+
+	private put(hero: Hero) {
+		let headers = new Headers()
+		headers.append('Content-Type', 'application/json')
+
+		let url = `${this.heroesUrl}/${hero.id}`
+
+		return this.http
+			.put(url, JSON.stringify(hero), {headers: headers})
+			.toPromise()
+			.then(() => hero)
+			.catch(this.handleError)
+	}
+
+	private handleError(error: any) {
+		console.error('An error occured', error)
+		return Promise.reject(error.message || error)
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
+import { XHRBackend } from '@angular/http'
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api'
+import { InMemoryDataService } from './app/in-memory-data.service'
 
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic'
+import { HTTP_PROVIDERS } from '@angular/http'
+import { enableProdMode } from '@angular/core'
 
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app.component'
 import { APP_ROUTER_PROVDIERS } from './app/app.routes'
 
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
 
-bootstrap(AppComponent, [APP_ROUTER_PROVDIERS]);
+bootstrap(AppComponent, [
+	APP_ROUTER_PROVDIERS,
+	HTTP_PROVIDERS,
+	{ provide: XHRBackend, useClass: InMemoryBackendService },
+	{ provide: SEED_DATA, useClass: InMemoryDataService }
+]);
